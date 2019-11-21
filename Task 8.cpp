b35e51f44d9b124eabc83c3e68ebad3913ba2a01
elloo98 <57644949+elloo98@users.noreply.github.com>
2019-11-21 18:48:31 +0100

    Add files via upload

---
+++ Task 8.cpp
@@ -0,0 +1,17 @@
+/*
+What did I learn? To properly handle vectors and to generarly orient myself in Visual Studio and to a lesser extent GitHub. I also learned that I should reconsider my extensive use of while loops.
+I learend to code using only while and if and learned about for and switch cases later. I know that switch cases are way better than if statements so I use them in longer lists, but for loops have
+inconsistent syntax across languages so I prefer while loops, but they are easier to screw up and have loop infinitley, especially if you nest something like six or seven whiles in eachother like in
+task 6
+
+What was difficult in the lab? The delete entries of task 6 was definitivley the hardest part of the asignment, probably because of my unfamiliarity with vectors. It probably consumed half of the 
+time spent on the entire asignment. It was coded in three spurts with three completley different solutions. The first was written with the rest of the program in its entirety without debugging and
+while it worked great for the rest of the program, it just crashed when I tried to delete something. The second time the code was copy-pasted from the search entries feature with some slight
+modifications. When this also crashed with the same exit code I took it really slow in building the feature, testing everything after every couple of lines added. This worked almost perfectly (there is
+still a NULL left behind from the deleted entry that I couldn't figure outh how to get rid of) but it is the reason why the feature took so long to program. There were too many issues with the feature
+to begin with so detailing the work that went into making it work is a task that I don't even remember in detail
+
+How can the lab be improved? The workload could be better distributed between the tasks
+
+How many hours? roughly 20
+*/
